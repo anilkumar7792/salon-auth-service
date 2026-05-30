b1820654fc33c8a2dd5bff5a58e21d0926bd8092
Anil Kumar <anilbaral7792@gmail.com>
2026-05-30 21:53:29 +0530

    first commit

---
+++ src/main/java/auth/salon/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package auth.salon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+
+}
